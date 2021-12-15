4ed06bd592836f87482be1e0aee029a42cd524b5
Dorota Stach <jdoe@example.com>
2021-12-15 17:29:07 +0100

    Changes

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -16,3 +16,45 @@ public class test : MonoBehaviour
         
     }
 }
+
+namespace QuickType
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Welcome
+    {
+        [JsonProperty("greeting")]
+        public string Greeting { get; set; }
+
+        [JsonProperty("instructions")]
+        public string[] Instructions { get; set; }
+    }
+
+    public partial class Welcome
+    {
+        public static Welcome FromJson(string json) => JsonConvert.DeserializeObject<Welcome>(json, QuickType.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Welcome self) => JsonConvert.SerializeObject(self, QuickType.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
