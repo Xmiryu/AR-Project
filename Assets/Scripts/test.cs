26444d4040d2fef480398d7c7a4657d31b59a5c5
Dorota Stach <jdoe@example.com>
2022-01-12 17:55:55 +0100

    Update Panel

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -9,7 +9,12 @@ using TMPro;
 public class test : MonoBehaviour
 {
     string path = "Assets/jsonformatter.txt";
-    [SerializeField] GameObject textPanel;
+    int stepNo = 0;
+
+
+    [SerializeField] GameObject title;
+    [SerializeField] GameObject description;
+    [SerializeField] GameObject stepNumber;
 
     Test Write()
     {
@@ -20,15 +25,40 @@ public class test : MonoBehaviour
         return txt;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        textPanel.GetComponent<TextMeshProUGUI>().text = Write().Author.Username;
+        title.GetComponent<TextMeshProUGUI>().text = Write().Title.ToString();
+        updateContent();
+
+    }
+
+    public void nextButton() 
+    {
+        stepNo += 1;
+        updateContent();
+    }
+    public void backButton() 
+    {
+        stepNo -= 1;
+        updateContent();
+    }
+
+    public void updateContent()
+    {
+        string descr = "";
+        for(int i = 0; i < Write().Steps[stepNo].Lines.Length; i++)
+        {
+            descr += $"{i+1}. {Write().Steps[stepNo].Lines[i].TextRaw} \n\n";
+        }
+        
+        description.GetComponent<TextMeshProUGUI>().text = descr;
+
+        stepNumber.GetComponent<TextMeshProUGUI>().text = "Step Number: " + (stepNo+1).ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
